64c92da130431b11e661c4e3aeb3c7c023b036a9
dylan9b <dylan9b@gmail.com>
2023-08-24 17:24:34 +0200

    Updated notes list to make use of angular/material.

--- src/app/notes/list/notes-list.module.ts
+++ src/app/notes/list/notes-list.module.ts
@@ -5,9 +5,21 @@ import { HeaderModule } from 'src/app/header/header.module';
 import { NotesListComponent } from './notes-list.component';
 import { MatRippleModule } from '@angular/material/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  imports: [CommonModule, RouterModule, HeaderModule, MatRippleModule, MatButtonModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    HeaderModule,
+    MatRippleModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule
+  ],
   declarations: [NotesListComponent],
   providers: [],
   bootstrap: [NotesListComponent],
